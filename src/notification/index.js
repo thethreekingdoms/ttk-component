5c42d0b2bb0261d0534bf9e1f57996ee945f6e36
guanyu <zhuliangcan@foresee.com.cn>
2018-09-05 15:21:33 +0800

    notification组件

--- src/notification/index.js
+++ src/notification/index.js
@@ -1,3 +1,23 @@
-import { notification } from 'antd';
+import { notification } from 'antd'
 
-export default notification;
+function promiseWrapper(fun) {
+	return (props) => {
+		return new Promise((resolve, reject) => {
+			let handleClose = () => {
+				resolve(true)
+			}
+			props.onClose = handleClose
+			fun(props)
+		})
+	}
+}
+
+export default {
+    config: notification.config,
+	open: promiseWrapper(notification.open),
+	success: promiseWrapper(notification.success),
+	error: promiseWrapper(notification.error),
+	info: promiseWrapper(notification.info),
+	warning: promiseWrapper(notification.warning),
+	warn: promiseWrapper(notification.warn),
+}
